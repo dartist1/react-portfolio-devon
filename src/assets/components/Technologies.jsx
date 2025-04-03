bd53eccc9392b462e75018ef26a0f00e241c5b0b
Devon <devon.artist@gmail.com>
2025-04-02 21:17:36 -0700

    first commit

---
+++ src/assets/components/Technologies.jsx
@@ -0,0 +1,52 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaPhp } from "react-icons/fa";
+import { FaAngular } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { motion } from "motion/react";
+import { animate } from "motion";
+
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: { 
+        y: [10, - 10], 
+        transition: { 
+            duration : duration, 
+            ease: "linear", 
+            repeat: Infinity, 
+            repeatType: "reverse",  
+        },
+    },
+})
+
+
+const Technologies = () => {
+  return (
+  <div className="border-b border-neutral-800 pb-24">
+        <motion.h1 whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} transition={{ duration: 1.5 }} className="my-20 text-center text-4xl">Technologies</motion.h1>
+        <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div variants={iconVariants(2.4)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <RiReactjsLine className="text-7xl text-cyan-400" />
+            </motion.div>
+            <motion.div variants={iconVariants(3)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <IoLogoJavascript  className="text-7xl text-yellow-400" />
+            </motion.div>
+            <motion.div variants={iconVariants(5)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaPhp className="text-7xl" />
+            </motion.div>
+            <motion.div variants={iconVariants(1.8)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaAngular  className="text-7xl text-green-500" />
+            </motion.div>
+            <motion.div variants={iconVariants(6)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <SiTypescript  className="text-7xl text-red-700" />
+            </motion.div>
+            <motion.div variants={iconVariants(2.9)} initial="initial" animate="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
+                <SiMysql  className="text-7xl text-sky-700" />
+            </motion.div>
+        </motion.div>
+    </div>
+  );
+}
+
+export default Technologies
---
+++ src/constants/index.js
@@ -0,0 +1,61 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+
+export const HERO_CONTENT = `Results-driven and detail-oriented Frontend Software Engineer with 5+ years of experience specializing in JavaScript, React, and scalable web applications. Proven expertise in delivering high-performance, customer-facing applications and collaborating with cross-functional teams in agile environments. Passionate about optimizing web performance and delivering seamless user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Application Engineer",
+    company: "DevonWay",
+    description: `Utilized SQL for database-driven applications, integrating data with RESTful APIs, improving data accuracy and performance.`,
+    technologies: ["SQL", "Cypress"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Capital One",
+    description: `Spearheaded development of scalable, maintainable web applications using JavaScript, Angular, and React, driving 25% faster load times and optimizing overall application performance.`,
+    technologies: ["HTML", "CSS", "Angular", "Java", "TypeScript"],
+  },
+  {
+    year: "2018 - 2022",
+    role: "Software Developer",
+    company: "University System of Maryland",
+    description: `Collaborated with product managers and UI/UX designers to create user-friendly interfaces and optimal user experiences utilizing PHP, SQL.`,
+    technologies: ["PHP", "HTML", "SQL", "CSS", "JavaScript"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Spotify Clone",
+    image: project1,
+    description:
+      "A fully functional Spotify Clonee with features like playlist, pausing and playing music, and user authentication.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "TailWindCSS"],
+  },
+  {
+    title: "Simple Shooter",
+    image: project3,
+    description:
+      "A simple shooting game built in Python.",
+    technologies: ["Python", "PyGame"],
+  },
+];
+
+export const CONTACT = {
+  address: "Las Vegas, NV ",
+  phoneNo: "2012805152 ",
+  email: "devon.artist@gmail.com",
+};
